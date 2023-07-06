dd5e6b181c34384b09875c75c7e3bd00c0daf268
Karla Teves <klitatk.19.96@gmail.com>
2023-07-05 23:25:56 -0500

    hash

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,7 +1,36 @@
 import { conexion } from "../base_de_datos.js";
+import { registroUsuarioDto} from '../dtos/usuario.dto.js'
+import bcrypt from 'bcryptjs'
 
-export const registroUsuario = (req, res) => {
-    res.status(201).json({
-        message: "usuario creado exitosamente"
+export const registroUsuario = async (req, res) => {
+    const { body } = req
+    const dataValidada = registroUsuarioDto.validate(body)
+
+    if (dataValidada.error){
+        return res.status(400).json({
+            message : 'Error al crear el usuario',
+            content: dataValidada.error.details
+        })
+    }
+
+    const salt = await bcrypt.genSalt()
+
+    const password = await bcrypt.hash(dataValidada.value.password, salt)
+
+    try{
+    const usuarioCreado = await conexion.usuario.create({
+        data : {...dataValidada.value, password}
     })
+
+    res.status(201).json({
+        message: "usuario creado exitosamente",
+        content : usuarioCreado
+    })
+}
+catch(error){
+    return res.status(500).json({
+        message: 'Error al crear el usuario',
+        content: 'El usuario ya existe'
+    })
+}
 }
